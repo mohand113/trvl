fb1b1fc6f63145e3b16d2db8fa6a0cfaf1ae69f1
unknown <amchichemiao4@gmail.com>
2024-04-07 14:31:36 +0300

    free vrfy

--- auth-server/routes/auth.routes.js
+++ auth-server/routes/auth.routes.js
@@ -123,15 +123,15 @@ router.post('/login', (req, res, next) => {
  
  
 // GET  /auth/verify
-// router.get('/verify', isAuthenticated, (req, res, next) => {       // <== CREATE NEW ROUTE
+ router.get('/verify', isAuthenticated, (req, res, next) => {       // <== CREATE NEW ROUTE
  
 //     // If JWT token is valid the payload gets decoded by the
 //     // isAuthenticated middleware and made available on `req.payload`
-//     console.log(`req.payload`, req.payload);
+     console.log(`req.payload`, req.payload);
    
 //     // Send back the object with user data
 //     // previously set as the token payload
-//     res.status(200).json(req.payload);
-  // });
+     res.status(200).json(req.payload);
+   });
 
 module.exports = router;
