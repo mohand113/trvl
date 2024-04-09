669b0211edd4b2f3f273ed2ecf0075be3a14d630
unknown <amchichemiao4@gmail.com>
2024-04-10 00:21:06 +0300

    auth-pst

--- auth-server/routes/post.routes.js
+++ auth-server/routes/post.routes.js
@@ -48,7 +48,7 @@ router.get("/posts", (req, res, next) => {
 	//	post.username = res.username;
 	//	console.log(Aposts);
             });
-      console.log(res);
+      console.log(allposts);
 	  res.json(allposts);
     })
     .catch((err) => {
