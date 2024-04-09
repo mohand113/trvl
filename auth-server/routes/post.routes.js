8d559ca545a44d98041c5398b4642470cb183b6d
unknown <amchichemiao4@gmail.com>
2024-04-09 20:39:22 +0300

    unm trblst2

--- auth-server/routes/post.routes.js
+++ auth-server/routes/post.routes.js
@@ -39,7 +39,7 @@ router.get("/posts", (req, res, next) => {
     // .populate("comments")
     .then((allposts) => {
         let posts = allposts;
-		let Aposts =[,];
+		let ArrayList Aposts = new ArrayList();
         posts.map( async (post) => {
 		const res = await User.findById(post.author);
 		Aposts.append([post,res.username]);
